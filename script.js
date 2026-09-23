43876c4f4caf896ab617b72fc494456bcccd40ac
zobaidah mohammady <zobaidahm@gmail.com>
2026-09-23 15:56:18 +0430

    js finished

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+});
+
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("open");
+  });
+});
